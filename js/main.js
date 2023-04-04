574fd9873408772fd662bd69eea3d9a6b781756e
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-04-04 08:00:43 +0600

    Задание 2

--- js/main.js
+++ js/main.js
@@ -1,6 +1,19 @@
 console.log(document.getElementById('string-3').textContent);
-console.log(document.getElementById('string-5').textContent); 
-console.log(document.getElementById('string-2').textContent); 
-console.log(document.getElementById('string-6').textContent); 
-console.log(document.getElementById('string-4').textContent); 
-console.log(document.getElementById('string-1').textContent);  
+console.log(document.getElementById('string-5').textContent);
+console.log(document.getElementById('string-2').textContent);
+console.log(document.getElementById('string-6').textContent);
+console.log(document.getElementById('string-4').textContent);
+console.log(document.getElementById('string-1').textContent);
+
+const divs = document.getElementsByClassName('element')
+for (i in divs) {
+    console.log(i);
+    if (i < 3){
+        divs[i].style.color = 'red' 
+        console.log(divs[i]);
+    }
+    else{
+        divs[i].style.color = 'green' 
+        console.log(divs[i]);
+    }  
+}
