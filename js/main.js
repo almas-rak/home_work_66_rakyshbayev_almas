010ad44a1314f97f14a83a12adb2eb484323e17b
Rakyshbaev Almas <kasper10.0711@gmail.com>
2023-04-04 08:44:42 +0600

    Задание 3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// task#1
 console.log(document.getElementById('string-3').textContent);
 console.log(document.getElementById('string-5').textContent);
 console.log(document.getElementById('string-2').textContent);
@@ -5,15 +6,25 @@ console.log(document.getElementById('string-6').textContent);
 console.log(document.getElementById('string-4').textContent);
 console.log(document.getElementById('string-1').textContent);
 
+//task#2
 const divs = document.getElementsByClassName('element')
 for (i in divs) {
-    console.log(i);
     if (i < 3){
         divs[i].style.color = 'red' 
-        console.log(divs[i]);
     }
-    else{
+    else if(i > 3 | i < 5){
         divs[i].style.color = 'green' 
-        console.log(divs[i]);
     }  
+}
+
+
+// task#3
+const container = document.getElementsByClassName('container')
+for(let i = 1; i < 6; i++){
+    const div = document.createElement('div')
+    const p = document.createElement('p')
+    p.innerText = `Element ${i}`
+    div.append(p)
+    container[0].append(div)
+    
 }
